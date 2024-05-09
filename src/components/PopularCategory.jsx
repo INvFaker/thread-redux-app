e9c0139278d2a8008eb75172860ac0e0033d1798
invfaker <dakun340@gmail.com>
2024-05-09 08:11:37 +0700

    delt comment line

--- src/components/PopularCategory.jsx
+++ src/components/PopularCategory.jsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import CategoryItem from './CategoryItem';
-
-// function PopularCategory({ categories, changeCategory }) {
-//   return (
-//     <header className="pb-4">
-//       <p className="text-white text-xl font-bold">Kategori popular</p>
-//       <div className="flex gap-3 mt-4">
-//         {/* {
-//           categories.values.map((category) => (
-//             <CategoryItem key={category} changeCategory={changeCategory} category={category} />))
-//         } */}
-//         {console.log(categories)}
-//       </div>
-//     </header>
-//   );
-// }
-
-// PopularCategory.propTypes = {
-//   categories: PropTypes.array.isRequired,
-//   changeCategory: PropTypes.func.isRequired,
-// };
-
-// export default PopularCategory;
-
 import React from 'react';
 import PropTypes, { string } from 'prop-types';
 import CategoryItem from './CategoryItem';
