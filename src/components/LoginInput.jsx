0cdf379d6ee4d79a4c866b0c5f4f7f1d58ef4f43
invfaker <dakun340@gmail.com>
2024-05-16 11:48:50 +0700

    component test

--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -9,7 +9,7 @@ function LoginInput({ login }) {
   return (
     <form action="" className="flex flex-col gap-1 mt-8 text-black">
       <input type="email" value={email} onChange={onEmailChange} placeholder="Email" className="px-2 py-3 border border-black rounded" />
-      <input type="password" value={password} onChange={onPasswordChange} name="" id="" placeholder="Password" className="p-2 border border-black rounded" />
+      <input type="password" value={password} onChange={onPasswordChange} placeholder="Password" className="p-2 border border-black rounded" />
       <button type="button" onClick={() => login({ email, password })} className="p-2 bg-blue-500 rounded text-white font-medium">Login</button>
     </form>
   );
